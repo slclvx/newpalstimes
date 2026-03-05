1e13484ecc7ee5e13274aaf7ab404c1cdc71c4fd
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:48:52 -0800

    Update comments.js

--- js/comments.js
+++ js/comments.js
@@ -1,1 +1,11 @@
+function post(){
 
+let text=document.getElementById("comment").value
+
+let div=document.createElement("div")
+
+div.innerText=text
+
+document.getElementById("comments").appendChild(div)
+
+}
