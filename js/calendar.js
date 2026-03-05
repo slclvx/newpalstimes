4a5540987e5a1722e1e7cc4d6d61215529c21589
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:49:54 -0800

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,19 @@
+const releases=[
 
+{name:"Hollow Knight Silksong",date:"April 12"},
+{name:"Metroid Prime 4",date:"June 1"},
+{name:"Starfield 2",date:"August 22"}
+
+]
+
+let cal=document.getElementById("calendar")
+
+releases.forEach(r=>{
+
+let div=document.createElement("div")
+
+div.innerText=r.name+" - "+r.date
+
+cal.appendChild(div)
+
+})
