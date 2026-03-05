d99ae6389ea15e5c9346db1d8bbc7e898b4a9a49
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:46:49 -0800

    Update wordle.js

--- js/wordle.js
+++ js/wordle.js
@@ -1,1 +1,31 @@
+const answer="mario"
 
+const grid=document.getElementById("grid")
+
+for(let i=0;i<30;i++){
+
+let tile=document.createElement("div")
+
+tile.className="tile"
+
+grid.appendChild(tile)
+
+}
+
+function checkGuess(){
+
+let guess=document.getElementById("guess").value.toLowerCase()
+
+if(guess===answer){
+
+alert("Correct!")
+
+}
+
+else{
+
+alert("Try again")
+
+}
+
+}
