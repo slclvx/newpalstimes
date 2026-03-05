9990a895fc9b29f7ae750a9901631bfd0897ab86
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:38:20 -0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,15 @@
+const articles=[
+"New Nintendo Console Rumors",
+"Top Indie Games of 2026",
+"AI in Game Development"
+]
 
+function search(){
+
+let q=document.getElementById("search").value.toLowerCase()
+
+let results=articles.filter(a=>a.toLowerCase().includes(q))
+
+alert("Results:\n"+results.join("\n"))
+
+}
