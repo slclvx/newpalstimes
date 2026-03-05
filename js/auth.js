68d20192ca431770e502b4bff6fd2f4197d69407
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:49:24 -0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+function signup(){
 
+let user=document.getElementById("user").value
+
+let pass=document.getElementById("pass").value
+
+localStorage.setItem(user,pass)
+
+alert("Account created")
+
+}
