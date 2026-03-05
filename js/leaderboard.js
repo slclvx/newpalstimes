d3ada132feb9789f744ded1c7a21dde2afeb6ebb
slclvx <s.lozano.colm@gmail.com>
2026-03-05 09:47:34 -0800

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,5 +1,21 @@
-<h1>Leaderboard</h1>
+let players=[
 
-<ul id="board"></ul>
+{name:"Sebastian",score:9000},
+{name:"Alex",score:5000},
+{name:"Jordan",score:4200}
 
-<script src="../js/leaderboard.js"></script>
+]
+
+players.sort((a,b)=>b.score-a.score)
+
+let list=document.getElementById("board")
+
+players.forEach(p=>{
+
+let li=document.createElement("li")
+
+li.innerText=p.name+" - "+p.score
+
+list.appendChild(li)
+
+})
